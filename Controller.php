9a060d557ac891fb56b2a591b773180b5621cf76
rohila95 <rohila95@gmail.com>
2017-10-20 21:50:02 -0400

    minor changes in controller

--- Controller.php
+++ Controller.php
@@ -1,24 +1,22 @@
 <?php
 
-include("ReactionService.php");
+
+include_once "./services/ReactionService.php";
 
 $reactionService = new ReactionService();
 
 if(isset($_POST["setReaction"])){
 
-		# Get JSON as a string
-	$json_str = file_get_contents('php://input');
-	# Get as an object
-	$json_obj = json_decode($json_str);
-	echo print_r($json_obj);
-	//return $reactionService.postReaction();
+
+	$reactionService -> postReaction();
+
+	$postInput={};
+	foreach($_POST as $key => $value){
+
+	}
+
+	
 }
-
-
-
-
-
-
 
 
 ?>
