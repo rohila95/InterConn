a1321ec3fd0448146d4e0566c5762823256e04b5
mgunn001 <mgunnam@cs.odu.edu>
2017-10-24 13:43:24 -0400

    reaction through ajax working

--- services/ReactionService.php
+++ services/ReactionService.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include_once("database_connect.php");
 include_once("SqlService.php");
 
@@ -7,12 +6,12 @@ include_once("SqlService.php");
 class ReactionService{
 
 // Method to take post a reaction to a particular message
-  public function postReactionIfNotExist($postInputObj)
+  public function postReactionIfNotExist($postInputObj,$useridFromSession)
   {
-      print_r($postInputObj);
+      //print_r($postInputObj);
 
-      echo "hello". $_SESSION['userid'];
-      $userid = $_SESSION['userid'];
+       //echo "hello".$useridFromSession;
+      $userid = $useridFromSession;
      $database_connection = new DatabaseConnection();
      $conn = $database_connection->getConnection();
      $userid = mysqli_real_escape_string($conn,$userid);
@@ -22,19 +21,22 @@ class ReactionService{
       $sql_service = new SqlService();
 
       $getSpecificMessageReactionSQLQuery = $sql_service->getSpecificMessageReaction($userid, $messageid,$emojid);
-
+      //echo $getSpecificMessageReactionSQLQuery;
      $result = $conn->query($getSpecificMessageReactionSQLQuery);
      if ($result->num_rows > 0) { // meaning there exist the same reaction already
 
+         // delete the existing Reaction
          $deleteMessageReactionSQLQuery =  $sql_service->deleteIfMessageReactionExist($userid, $messageid,$emojid);
+         //echo $deleteMessageReactionSQLQuery;
          $conn->query($deleteMessageReactionSQLQuery);
 
-         if ($conn->affected_rows() > 0) {
+         if ($conn->affected_rows > 0) {
              return "success-deleted";
          }
      } else { // there isn't this reaction by the current user, insert one
          $timestamp = date('Y-m-d H:i:s', time());
          $insertMessageReactionSQLQuery =  $sql_service->insertMessageReaction($userid, $messageid,$emojid,$timestamp);
+         //echo $insertMessageReactionSQLQuery;
          $isInserted = $conn->query($insertMessageReactionSQLQuery);
          if ($isInserted === TRUE) {
              return "success-inserted";
