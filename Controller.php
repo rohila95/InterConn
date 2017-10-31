021990ab790d01d4c6567e323ca2ffc3171fa932
Maheedhar Gunnam <maheedhargunnam@gmail.com>
2017-10-31 17:40:02 -0400

    update task tracker

--- Controller.php
+++ Controller.php
@@ -7,13 +7,14 @@ include_once "./services/ReactionService.php";
 include_once "./services/WebService.php";
 $postInputObj= [];
 
-// echo "hello";
-// to read the $_Post incoming values in to an object
-if($_POST){
-    foreach($_POST as $key => $value){
-        $postInputObj[$key] = $value;
+    // echo "hello";
+    // to read the $_Post incoming values in to an object
+    if($_POST){
+        foreach($_POST as $key => $value){
+            $postInputObj[$key] = $value;
+        }
     }
-}
+
 $reactionService = new ReactionService();
 $webService = new WebService();
 if(isset($_POST["setReaction"])){ // to post a react by a
